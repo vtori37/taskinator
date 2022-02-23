45b7f1a92c16fd0458cadf9f971ae0f4c4c63276
Tori Acer <vrice360@gmail.com>
2022-02-22 20:03:29 -0600

    added if statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,19 @@ var taskFormHandler = function(event) {
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
   
     // package up data as an object
-    var taskDataObj = {
-        name: taskNameInput,
-        type: taskTypeInput
-    };
-  
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+  }
+
+    formEl.reset();
+
+
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
     // send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
   };
